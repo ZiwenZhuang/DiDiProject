ebc44046bd866572a1e84980e3cad1101d32b85f
ZiwenZhuang <leo416850130@outlook.com>
2018-05-21 19:31:07 +0800

    open file with fstream

--- codes/main.cpp
+++ codes/main.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
+#include <string>
 #include <fstream>
 
 using namespace std;
 
 int main() {
     printf("start the program\n");
-    FILE* data_file = fopen("./data/order_20161101", "r");
 
+    fstream order; order.open("./data/order_20161101", ios_base::in);
+
+    string one_line;
+    order >> one_line;
+    printf(one_line.data()); printf("\n");
 
     printf("press any key and Enter to quit\n");
     char q;

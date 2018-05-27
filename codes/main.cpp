90d9b53673e41219debd8fbbd35bdc9174f89ef4
ZiwenZhuang <leo416850130@outlook.com>
2018-05-27 23:34:47 +0800

    change the read methods

--- codes/main.cpp
+++ codes/main.cpp
@@ -7,14 +7,14 @@ using namespace std;
 int main() {
     cout << "start the program\n";
 
-    fstream order_file; order_file.open("../data/order_20161101", ios_base::in);
+    //fstream order_file; order_file.open("../data/order_20161101", ios_base::in);
     fstream gps_file; gps_file.open("../data/gps_20161101", ios_base::in);
 
     string one_line;
-    fstream& file2read = order_file;
+    fstream& file2read = gps_file;
     for (int i = 0; i < 12; i++) {
-        getline(file2read, one_line);
-        cout << one_line << endl;
+        file2read >> one_line;
+        cout << "Read one Line |" << one_line << endl;
     }
 
     printf("press any key and Enter to quit\n");

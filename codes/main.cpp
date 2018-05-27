2dbecea9ee787840301bf6a4353329cd675d15c7
ZiwenZhuang <leo416850130@outlook.com>
2018-05-28 00:38:57 +0800

    close file at the end

--- codes/main.cpp
+++ codes/main.cpp
@@ -10,14 +10,14 @@ int main() {
     //fstream order_file; order_file.open("../data/order_20161101", ios_base::in);
     fstream gps_file; gps_file.open("../data/gps_20161101", ios_base::in);
 
-    string one_line = "";
+    string one_line;
     for (int i = 0; i < 12; i++) {
         gps_file >> one_line;
         cout << "Read one Line |" << one_line << endl;
     }
 
     printf("press any key and Enter to quit\n");
-    char q;
-    cin >> q;
+    char q; cin >> q;
+    order_file.close(); gps_file.close();
     return 0;
 }

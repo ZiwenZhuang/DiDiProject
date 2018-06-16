91836e393441d3bc3e8f5fcb050b41ce98ac7f8d
wangyh1 <wangyh1@shanghaitech.edu.cn>
2018-06-16 15:34:58 +0800

    KNNremoval

---
+++ codes/utilities/KNNFloatImg.h
@@ -0,0 +1,21 @@
+#ifndef KNNFLOAT_H
+#define KNNFLOAT_H
+
+#include <iostream>
+#include <cstdlib>
+#include <cstdint>
+#include <string>
+#include <algorithm>
+#include <fstream>
+#include <sstream>
+
+#include <math.h>
+#include <chrono>
+#include <omp.h>
+
+using namespace std;
+
+void KNN_removal(const float * imgIn, int * imgOut, const unsigned int &width, const unsigned int &height, const unsigned int &kernelSize, const unsigned int k, const float distance_thr);
+
+
+#endif

33be244e519cd28496a33ed2c5e9d94c2498cb3b
ZiwenZhuang <leo416850130@outlook.com>
2018-06-18 16:08:10 +0800

    updating main function

--- codes/main.cpp
+++ codes/main.cpp
@@ -21,32 +21,66 @@ string read_a_line(fstream& file) {
     return one_line;
 }
 
-int main() {
+void usage() {
+    cout << "please specify the usage in the next argument\n";
+    cout << "\t'raw': only write the raw image\n";
+    cout << "\t'enhance': use simple enhance kernel and do the comvolution, write result to 'paths_enhanced.pgm'\n";
+    cout << "\t'two_level': split pixels in only 0 or 1, write result into 'two_level.pgm'\n";
+    cout << "\t'knn': applying knn_removal method, write result into 'knn_removal_result.pgm'\n";
+    cout << "\t'anios': applying Aniostropic diffusion method, write result into 'anios_diff.pgm'\n";
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        usage();
+        printf("The program ends\n\tpress any key and Enter to quit\n");
+        char q; cin >> q;
+        return 0;
+    }
     cout << "start the program\n";
 
     fstream order_file; order_file.open("../data/order_20161101", ios_base::in);
     fstream gps_file; gps_file.open("../data/gps_20161101", ios_base::in);
 
-    // write directly the readin image from Lottie
+    
     Matrix<int> paths_img = Matrix<int>(720, 480);
     paths_img.set_data(pixel(gps_file, \
         paths_img.getRowNum(), paths_img.getColNum()));
-    pgm_ASCII::write_image<int>(paths_img, "../data/paths_img.pgm");
+    if (strcmp(argv[0], "raw") == 0) {
+        // write directly the readin image from Lottie
+        pgm_ASCII::write_image<int>(paths_img, "../data/paths_img.pgm");
+        return 0;
+    }
 
-    // Using edge detection kernel to enhance the image. The kernel is in the cooresponding function.
-    Matrix<int> temp_img;
-    sharpen_matrix(paths_img, temp_img);
-    pgm_ASCII::write_image<int>(temp_img, "../data/paths_enhanced.pgm");
+    if (strcmp(argv[0], "enhance") == 0) {
+        // Using edge detection kernel to enhance the image. The kernel is in the cooresponding function.
+        Matrix<int> temp_img (paths_img.getRowNum(), paths_img.getColNum());
+        sharpen_matrix(paths_img, temp_img);
+        pgm_ASCII::write_image<int>(temp_img, "../data/paths_enhanced.pgm");
 
+    } else if (strcmp(argv[0], "two_level") == 0) {
+        // Set the image with only two value either 0 or 1, the threshold is in the function.
+        Matrix<int> temp_img (paths_img.getRowNum(), paths_img.getColNum());
+        two_level(paths_img, temp_img);
+        pgm_ASCII::write_image<int>(temp_img, "../data/two_level.pgm");
 
-    // Set the image with only two value either 0 or 1, the threshold is in the function.
-    two_level(paths_img, temp_img);
-    // // Using KNN removal method try to make the process clear.
-    Matrix<int> tempp_img;
-    sharpen_matrix(paths_img, tempp_img);
-    knn_removal(temp_img, tempp_img);
-    pgm_ASCII::write_image<int>(tempp_img, "../data/knn_removal_result.pgm");
-    //pgm_ASCII::write_image<int>(temp_img, "../data/two_level.pgm");
+    } else if (strcmp(argv[0], "knn") == 0) {
+        // Using KNN removal method try to make the process clear.
+        Matrix<int> temp_img (paths_img.getRowNum(), paths_img.getColNum());
+        Matrix<int> tempp_img (paths_img.getRowNum(), paths_img.getColNum());
+        two_level(paths_img, temp_img);
+        knn_removal(temp_img, tempp_img);
+        pgm_ASCII::write_image<int>(tempp_img, "../data/knn_removal_result.pgm");
+
+    } else if (strcmp(argv[0], "anios") == 0) {
+        // Apply Aniostropic diffusion method to try to eliminate the noise
+        Matrix<int> temp_img (paths_img.getRowNum(), paths_img.getColNum());
+        anios_diff(paths_img, temp_img);
+        pgm_ASCII::write_image<int>(temp_img, "../data/anios_diff.pgm");
+
+    } else {
+        usage();
+    }
 
     printf("The program ends\n\tpress any key and Enter to quit\n");
     char q; cin >> q;
--- codes/utilities/imageProcess.tpp
+++ codes/utilities/imageProcess.tpp
@@ -13,8 +13,6 @@ Matrix<int>& sharpen_matrix(Matrix<int> &mat_in, Matrix<int> &mat_out) {
     // perform convolution and check if it is sucess to store the result.
     if (convolve2D(mat_in.get_data(), data_out, mat_in.getColNum(), mat_in.getRowNum(),\
     kernel + 4, kernel_size_x, kernel_size_y)) {
-        mat_out.setRowNum(mat_in.getRowNum());
-        mat_out.setColNum(mat_in.getColNum());
         mat_out.set_data(data_out);
     } else {
         cerr << "Cannot perform convolution on the input matrix," << endl;
@@ -31,7 +29,6 @@ Matrix<int>& knn_removal(Matrix<int> &mat_in, Matrix<int> &mat_out) {
 Matrix<int>& two_level(Matrix<int> &mat_in, Matrix<int> &mat_out) {
     unsigned int threshold = 0; // The threshold that seperate the pixel to 0 or 1.
 	int* data_out = new int [mat_in.getRowNum() * mat_in.getColNum()];
-	mat_out.setRowNum(mat_in.getRowNum()); mat_out.setColNum(mat_in.getColNum());
 	mat_out.set_data(data_out);
 	for (int i = 0; i < mat_in.getRowNum(); i++) {
 		for (int j = 0; j < mat_in.getColNum(); j++) {

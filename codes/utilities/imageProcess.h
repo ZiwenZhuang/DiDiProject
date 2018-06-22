d4ab12b44eeaa50114a3ed9e3a08949341939650
ZiwenZhuang <leo416850130@outlook.com>
2018-06-22 20:08:41 +0800

    add a better demo

--- codes/utilities/imageProcess.h
+++ codes/utilities/imageProcess.h
@@ -44,7 +44,7 @@ which is used to compared with the distance of two coordinates. (In Manhantan Di
 If they are too far away, this function will return false directly.
 */
 bool check_connected(Matrix<int> &image, const std::pair<int, int> &nodeA, const std::pair<int, int> &nodeB\
-    , int threshold = 1, int line_width = 3, int maxSegments = 3, int maxLength = 100);
+    , int threshold = 1, int line_width = 3, int maxSegments = 2, int maxLength = 100);
 
 #include "imageProcess.tpp"
 
